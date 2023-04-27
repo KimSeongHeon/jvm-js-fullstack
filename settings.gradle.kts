6ec41306e1af9ffa6039ddb423594a13168bed12
KimSeongHeon <47879772+KimSeongHeon@users.noreply.github.com>
2023-04-27 10:23:13 +0900

    test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,8 @@
 pluginManagement {
-    resolutionStrategy {
-        eachPlugin {
-            println("::: each Plugin ${this.requested.id.id}")
-            if (requested.id.id == "org.jetbrains.kotlin.multiplatform") {
-                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
-            }
-        }
-    }
     repositories {
         mavenCentral()
         google()
         gradlePluginPortal()
-        maven { setUrl("https://plugins.gradle.org/m2/") }
     }
 }
 rootProject.name = "shoppinglist"
